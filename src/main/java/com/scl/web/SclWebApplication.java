15ae741e0adb766dab899613b0aa902555730b4b
Jinash Mandora <jinash.mandora@gmail.com>
2021-05-23 23:50:44 +0530

    updated with manual ack

--- src/main/java/com/scl/web/SclWebApplication.java
+++ src/main/java/com/scl/web/SclWebApplication.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 
 import com.scl.schema.Cat;
 
@@ -18,17 +20,22 @@ public class SclWebApplication {
 		SpringApplication.run(SclWebApplication.class, args);
 	}
 
-//	@Bean
-//	public Consumer<KStream<String, Cat>> process() {
-//		return input -> input.foreach((key, value) -> System.out
-//				.println("Key: " + key + " Value: " + value.toString()));
-//	}
+	// @Bean
+	// public Consumer<KStream<String, Cat>> process() {
+	// return input -> input.foreach((key, value) -> System.out
+	// .println("Key: " + key + " Value: " + value.toString()));
+	// }
 
 	@Bean
-	public Consumer<List<Cat>> batch() {
-		return messages -> messages.forEach(cat -> {
-			System.out.println("Cat => " + cat.toString());
-		});
+	public Consumer<Message<List<Cat>>> batch() {
+		return message -> {
+			Acknowledgment acknowledgment = message.getHeaders()
+					.get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
+			if (acknowledgment != null) {
+				System.out.println("Acknowledgment provided");
+				acknowledgment.acknowledge();
+			}
+		};
 	}
 
 	@Bean
